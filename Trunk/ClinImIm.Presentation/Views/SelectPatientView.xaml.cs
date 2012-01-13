2e81f9eb395efba2d8b6be8291368f72559f69c8
Andrew Newton <nootn@hotmail.com>
2012-01-13 23:33:40 +1000

    Created blank view to get UI running again

---
+++ Trunk/ClinImIm.Presentation/Views/SelectPatientView.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.Composition;
+using System.Windows.Controls;
+using ClinImIm.Applications.Views;
+
+namespace ClinImIm.Presentation.Views
+{
+    /// <summary>
+    /// Interaction logic for SelectPatientView.xaml
+    /// </summary>
+    [Export(typeof(ISelectPatientView)), PartCreationPolicy(CreationPolicy.NonShared)]
+    public partial class SelectPatientView : UserControl, ISelectPatientView
+    {
+        public SelectPatientView()
+        {
+            InitializeComponent();
+        }
+    }
+}
