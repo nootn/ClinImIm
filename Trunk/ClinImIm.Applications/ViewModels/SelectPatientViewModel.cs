fae7465cb7e715856f949ba0f4f28d342dfc6756
Andrew Newton <nootn@hotmail.com>
2012-01-13 20:37:43 +1000

    Created initial raw version for writing tests against.

---
+++ Trunk/ClinImIm.Applications/ViewModels/SelectPatientViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.ComponentModel.Composition;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Waf.Applications;
+using ClinImIm.Applications.Core;
+using ClinImIm.Applications.Views;
+using ClinImIm.Domain;
+
+namespace ClinImIm.Applications.ViewModels
+{
+    [Export, PartCreationPolicy(CreationPolicy.NonShared)]
+    public class SelectPatientViewModel : ViewModel<ISelectPatientView>
+    {
+        private Patient _model;
+
+        [ImportingConstructor]
+        public SelectPatientViewModel(ISelectPatientView view, Patient model)
+            : base(view)
+        {
+            _model = model ?? new Patient();
+        }
+
+        public Patient Model { get { return _model; } }
+
+        public void Reset()
+        {
+            _model = new Patient();
+        }
+    }
+}
---
+++ Trunk/ClinImIm.Domain/Patient.cs
@@ -0,0 +1,45 @@
+﻿using System.ComponentModel.Composition;
+using System.ComponentModel.DataAnnotations;
+
+namespace ClinImIm.Domain
+{
+    [Export]
+    public class Patient : ValidationModel
+    {
+        private string _identifier;
+        private string _fullName;
+
+        [ImportingConstructor]
+        public Patient()
+        {
+        }
+
+        [Required(ErrorMessage = "Patient must have an identifier")]
+        public string Identifier
+        {
+            get { return _identifier; }
+            set
+            {
+                if (_identifier != value)
+                {
+                    _identifier = value;
+                    RaisePropertyChanged("Identifier");
+                }
+            }
+        }
+
+        [Required(ErrorMessage = "Patient must have a full name")]
+        public string FullName
+        {
+            get { return _fullName; }
+            set
+            {
+                if (_fullName != value)
+                {
+                    _fullName = value;
+                    RaisePropertyChanged("FullName");
+                }
+            }
+        }
+    }
+}
