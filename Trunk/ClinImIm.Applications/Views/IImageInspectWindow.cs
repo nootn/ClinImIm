616f6af23536bdd4fa575e86f0d7f0f9e22e77cb
Jeff Burn <jeff@jeffburn.com>
2012-01-17 23:41:05 +1000

    Creates skeleton window ImageInspectWindow and corresponding interface

---
+++ Trunk/ClinImIm.Applications/Views/IImageInspectWindow.cs
@@ -0,0 +1,9 @@
+using System.Waf.Applications;
+
+namespace ClinImIm.Applications.Views
+{
+    public interface IImageInspectWindow : IView
+    {
+        
+    }
+}
---
+++ Trunk/ClinImIm.Presentation/Views/ImageInspectWindow.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.Composition;
+using System.Windows;
+using ClinImIm.Applications.Views;
+
+namespace ClinImIm.Presentation.Views
+{
+    /// <summary>
+    /// Interaction logic for ImageInspectWindow.xaml
+    /// </summary>
+    [Export(typeof(IImageInspectWindow))]
+    public partial class ImageInspectWindow : Window, IImageInspectWindow
+    {
+        public ImageInspectWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
